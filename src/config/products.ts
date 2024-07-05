747cdc25fe0eac9239fb9dc8d9dffdaab3162bd5
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-05 19:20:29 +0530

    integrate payment gateway

--- src/config/products.ts
+++ src/config/products.ts
@@ -1,12 +1,12 @@
 export const PRODUCT_PRICES = {
-    material: {
-        silicone: 0,
-        polycarbonate: 500,
-    },
-    finish: {
-        smooth: 0,
-        textured: 300,
-    }
-} as const
+  material: {
+    silicone: 0,
+    polycarbonate: 5000,
+  },
+  finish: {
+    smooth: 0,
+    textured: 3000,
+  },
+} as const;
 
-export const BASE_PRICE = 140
+export const BASE_PRICE = 1400;

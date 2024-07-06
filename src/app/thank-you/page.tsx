7b09dc7099dd1088b63deb7f2f1e4cffde2983fa
Rohit-2012 <rohit.kirti1219@gmail.com>
2024-07-06 12:40:07 +0530

    add thankyou-page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from 'react'
+import ThankYou from './ThankYou'
+
+
+const ThankYouPage = () => {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  )
+}
+
+export default ThankYouPage
